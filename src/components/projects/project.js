f23560171b23460bc136c9dbeccf78a181f25fed
Hasmik20 <61650766+Hasmik20@users.noreply.github.com>
2022-04-03 16:42:13 +0400

    add menu Reat

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -3,48 +3,54 @@ export const projects = [
  
   {
     id: 1,
+    image: "./img/menu.jpg",
+    title: "Food Menu with React",
+    link: "https://hasmik20.github.io/menuReact/"
+  },
+    {
+    id: 2,
     image: "./img/slideShow.png",
-    title: "Slide-Show with react",
+    title: "Slide-Show with React",
     link: "https://hasmik20.github.io/slide-show-react/"
   },
   {
-    id: 2,
+    id: 3,
     image: "./img/clock.png",
-    title: "Clock with react",
+    title: "Clock with React",
     link: "https://hasmik20.github.io/clock-react/"
   },
   {
-    id: 3,
+    id: 4,
     image: "./img/moveItem.png",
-    title: "Circle with react",
+    title: "Circle with React",
     link: "https://hasmik20.github.io/movingItemReact/"
   },
   {
-    id: 4,
+    id: 5,
     image: "./img/memoryGame.jpg",
     title: "Memory Game with JS",
     link: "https://hasmik20.github.io/memoryGameJs/"
   },
    {
-    id: 5,
+    id: 6,
     image: "./img/memoryReact.jpg",
     title: "Memory Game with React",
     link: "https://hasmik20.github.io/MemoryGameReact/"
   },
   {
-    id: 6,
+    id: 7,
     image: "./img/table.png",
     title: "Dynamic Table with JS",
     link: "https://hasmik20.github.io/dynamicTableJs/"
   },
    {
-    id: 7,
+    id: 8,
     image: "./img/fetchJS.png",
     title: "Post/Get (Fetch) with JS",
     link: "https://hasmik20.github.io/fetchJs/"
   },
    {
-    id: 8,
+    id: 9,
     image: "./img/formJs.png",
     title: "SignIn form with JS",
     link: "https://hasmik20.github.io/signInJS/"
