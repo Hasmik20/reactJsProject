dbe44bec7ac6b690aff4f50509e7185ef77b6969
Hasmik20 <61650766+Hasmik20@users.noreply.github.com>
2022-06-02 12:20:59 +0400

    add reactReduxShop

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -13,44 +13,50 @@ export const projects = [
     title: "Slide-Show with React",
     link: "https://hasmik20.github.io/slide-show-react/"
   },
-  {
+     {
     id: 3,
+    image: "./img/shop.png",
+    title: "Shop with React-Redux",
+    link: "https://hasmik20.github.io/ReactReduxShop/"
+  },
+  {
+    id: 4,
     image: "./img/clock.png",
     title: "Clock with React",
     link: "https://hasmik20.github.io/clock-react/"
   },
   {
-    id: 4,
+    id: 5,
     image: "./img/moveItem.png",
     title: "Circle with React",
     link: "https://hasmik20.github.io/movingItemReact/"
   },
   {
-    id: 5,
+    id: 6,
     image: "./img/memoryGame.jpg",
     title: "Memory Game with JS",
     link: "https://hasmik20.github.io/memoryGameJs/"
   },
    {
-    id: 6,
+    id: 7,
     image: "./img/memoryReact.jpg",
     title: "Memory Game with React",
     link: "https://hasmik20.github.io/MemoryGameReact/"
   },
   {
-    id: 7,
+    id: 8,
     image: "./img/table.png",
     title: "Dynamic Table with JS",
     link: "https://hasmik20.github.io/dynamicTableJs/"
   },
    {
-    id: 8,
+    id: 9,
     image: "./img/fetchJS.png",
     title: "Post/Get (Fetch) with JS",
     link: "https://hasmik20.github.io/fetchJs/"
   },
    {
-    id: 9,
+    id: 10,
     image: "./img/formJs.png",
     title: "SignIn form with JS",
     link: "https://hasmik20.github.io/signInJS/"
